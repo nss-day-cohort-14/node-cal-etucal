ef47ace94a3dc6aef77277d5dff32313cc25cdf0
Tim Creasy <timcreasy@me.com>
2016-09-01 11:07:38 -0500

    Finished print calendar module

--- test/print-month.js
+++ test/print-month.js
@@ -177,8 +177,3 @@ describe('Print Month', () => {
     });
   });
 });
-
-
-// 
-// expected '   February 2014\nSu Mo Tu We Th Fr Sa\n                   1\n 2  3  4  5  6  7  8\n 9 10 11 12 13 14 15\n16 17 18 19 20 21 22\n23 24 25 26 27 28\n' to equal
-// '   February 2014\nSu Mo Tu We Th Fr Sa\n                   1\n 2  3  4  5  6  7  8\n 9 10 11 12 13 14 15\n16 17 18 19 20 21 22\n23 24 25 26 27 28\n\n'
