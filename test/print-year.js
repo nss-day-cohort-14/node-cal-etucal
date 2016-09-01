022e7d85572a3d0b53246a0e709e7d18f1110eca
Tim Creasy <timcreasy@me.com>
2016-09-01 11:56:19 -0500

    Added tests for print year module

--- test/print-year.js
+++ test/print-year.js
@@ -9,21 +9,10 @@ describe('Print Year', () => {
   it('should be a function', () => {
     assert.isFunction(printYear);
   });
-  it.skip('should print a formatted year', (cb) => {
+  it('should print a formatted year', (cb) => {
     exec('cal 2015', (err, stdout) => {
       assert.strictEqual(printYear(2015), stdout.toString());
       cb();
     });
-  });
-  it('should add trailing spaces to each month line under 20 characters', (cb) => {
-    let monthString = "";
-    let monthArray = [];
-    for (let i = 1; i <= 12; i++) {
-      monthString += printMonth({year: 2016, month: i, day: 1});
-    }
-    monthArray = monthString.split('\n');
-    for (let i = 0; i < monthArray.length; i++) {
-      assert.strictEqual(monthArray[i].length, 20);
-    }
   });
 });
